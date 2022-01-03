18c88e3eaf8c9a6b0a2dc2d31a3014fa68756dff
rong <rongyy94job@gmail.com>
2022-01-03 15:42:52 +0800

    完成登入檢查

--- api/chk_pw.php
+++ api/chk_pw.php
@@ -1,6 +1,20 @@
 <?php include_once "../base.php"; 
 
 
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+
+$chk=$User->math('count','*',['acc'=>$acc,'pw'=>$pw]);
+
+// echo ($chk>0)?1:0;    三元運算寫法
+
+if($chk>0){
+    // $_SESSION['login']=$_POST['acc'];
+    echo 1;
+    $_SESSION['login']=$acc;
+}else{
+    echo 0;
+}
 
 
 
