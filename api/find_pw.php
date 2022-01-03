6a167133fd6f11e192acb2c8399a453d67be2a85
rong <rongyy94job@gmail.com>
2022-01-03 14:50:14 +0800

    完成找回密碼功能

--- api/find_pw.php
+++ api/find_pw.php
@@ -2,6 +2,14 @@
 
 
 
+$email=$_POST['email'];
+$user=$User->find(['email'=>$email]);
+
+if(empty($user)){
+    echo "查無此資料";
+}else{
+    echo "您的密碼為".$user['pw'];
+}
 
 
 
--- front/forget.php
+++ front/forget.php
@@ -3,14 +3,16 @@
     <div>請輸入信箱以查詢密碼</div>
     <div><input type="text" name="email" id="email"></div>
     <div></div>
-    <div><button>尋找</button></div>
-
-
-
-
-
-
-
+    <div><button onclick="find()">尋找</button></div>
 
 
 </fieldset>
+<script>
+    function find(){
+        $.post("api/find_pw.php",{email:$("#email").val()},(result)=>{
+            //不管結果是什麼都給我放在網頁上
+            $("#result").text(result)
+        })
+    }
+
+</script>
