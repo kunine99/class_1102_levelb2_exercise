cd1f473ead34194e4b03c3d69edd4a22dabe7e56
rong <rongyy94job@gmail.com>
2022-01-03 11:21:41 +0800

    建立主題內容及頁面

--- front/header.php
+++ front/header.php
@@ -1,5 +1,8 @@
 <div id="title">
-        <?=date("m 月 d 號 l");?> | 今日瀏覽: 1 | 累積瀏覽: 36        
+        <?=date("m 月 d 號 l");?> | 
+        <!-- 要測session瀏覽器要全部關掉再開 -->
+        今日瀏覽: <?=$View->find(['date'=>date("Y-m-d")])['total'];?> | 
+        累積瀏覽: <?=$View->math('sum','total');?>
 		<a href="index.php" style="float: right;">回首頁</a>
 	</div>
         <div id="title2">
