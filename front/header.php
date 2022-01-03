0aa29f256e886803b2e1cbf17711eb39ffeaf5ec
rong <rongyy94job@gmail.com>
2022-01-03 09:52:35 +0800

    完成切版及基礎檔案建立

---
+++ front/header.php
@@ -0,0 +1,9 @@
+<div id="title">
+        <?=date("m 月 d 號 l");?> | 今日瀏覽: 1 | 累積瀏覽: 36        
+		<a href="index.php" style="float: right;">回首頁</a>
+	</div>
+        <div id="title2">
+        	<a href="index.php" title="健康促進網-回首頁">
+				<img src="./icon/02B01.jpg" alt="" srcset="">
+			</a>
+        </div>
--- index.php
+++ index.php
@@ -13,15 +13,7 @@
 	<pre id="ssaa"></pre>
 </div>
 	<div id="all">
-    	<div id="title">
-        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36        
-		<a href="index.php" style="float: right;">回首頁</a>
-	</div>
-        <div id="title2">
-        	<a href="index.php" title="健康促進網-回首頁">
-				<img src="./icon/02B01.jpg" alt="" srcset="">
-			</a>
-        </div>
+    	<?php include "./front/header.php"; ?>
         <div id="mm">
         	<div class="hal" id="lef">
             	                	    <a class="blo" href="?do=po">分類網誌</a>
@@ -38,15 +30,21 @@
                     	                    	<a href="?do=login">會員登入</a>
                     	                    </span>
                     	<div class="">
-                		                        </div>
+<?php
+$do=$_GET['do']??'home';
+$file='front/'.$do.".php"; // 檔案名稱都放在file裡面?
+//如果這個檔案是存在的就把他include進來，不存在的話我預設都載入front hpme php
+if(file_exists($file)){
+	include $file;
+}else{
+	include "front/home.php";
+}
+?>
+                		</div>
                 </div>
             </div>
         </div>
-        <div id="bottom">
-    	    本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2022健康促進網社群平台 All Right Reserved 
-    		 <br>
-    		 服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
-        </div>
+		<?php include "./front/footer.php"; ?>
     </div>
 
 </body></html>
