4ebf4ce30b6ab5ff37a7ee06e8c5514e21517c50
rong <rongyy94job@gmail.com>
2022-01-10 11:20:32 +0800

    完成投票功能

---
+++ api/vote.php
@@ -0,0 +1,15 @@
+<?php include_once "../base.php";
+
+// 送出來是name=opt ，所以這邊也要opt
+$opt=$Que->find($_POST['opt']);
+$opt['count']++;
+
+$subject=$Que->find($opt['parent']);
+$subject['count']++;
+
+$Que->save($opt);
+$Que->save($subject);
+
+
+
+to("../index.php?do=que");
--- front/vote.php
+++ front/vote.php
@@ -1,23 +1,29 @@
 <!-- 投票 -->
 <?php
-
+$subject=$Que->find($_GET['id']);
 ?>
 <fieldset>
-<legend>目前位置</legend>  
-<h3></h3>  
-這兩個都是一樣的意思，都是我主題的選項內容
+    <legend>目前位置：首頁>問卷調查><?=$subject['text'];?></legend>
+    <h3><?=$subject['text'];?></h3>
+    <form action="api/vote.php" method="post">
+        <?php
 
-<?php
-foreach($options as $key =>$opt){
-?>
-<p>
-    <!-- 不要加陣列，因為我只要選一項 -->
+// 這兩個都是一樣的意思，都是我主題的選項內容
+//$options=$Que->all(['parent'=>$subject['id']])
+$options=$Que->all(['parent'=>$_GET['id']]);
+foreach($options as $key => $opt){
+    ?>
+    <p>
+        <!-- 不要加陣列，因為我只要選一項 -->
+        <input type="radio" name="opt" value="<?=$opt['id'];?>"><?=$opt['text'];?>
+    </p>
 
-</p>
+    <?php
+    }
+    ?>
+    <div class="ct">
+        <input type="submit" value="我要投票">
+    </div>
+</form>
 
-
-
-<?php
-}
-?>
 </fieldset>
